0dd5cd528a6ba4006db649b2290267d366d433ed
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-05 15:42:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,14 @@ var move=document.getElementById("image");
 
 var marginleft=10;
 function moveright(){
+    while(move.clicked=='false'){
    
     marginleft=marginleft+1;
     move.style.marginLeft=marginleft+ 'px';
 }
+}
 
 move.onclick=function (){
-   for(;move.clicked=='false';)
    
     var interval=setInterval(moveright,50);
            
