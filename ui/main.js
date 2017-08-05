d826d1dc54d9c22a2f40a7c9e7beb6fa9886809a
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-05 14:45:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,11 @@ var element=document.getElementById("main_text");
 
 element.innerHTML='Changed value using JS';
 var move=document.getElementById("image");
+
+var marginleft=10;
 function moveright(){
-    marginleft=10;
-    move.style.marginleft=marginleft+1;
+    marginleft=marginleft+10;
+    move.style.marginleft=marginleft+'px';
 }
 
 move.onclick=function (){
