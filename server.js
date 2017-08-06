59d951bdad76bdac4a6e636a541b84e1c05cceaf
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-06 16:22:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/counter', function (req, res) {
 
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
-  var artName=req.params.name;
+  var name=req.params.name;
   names.push[name];
   res.send(JSON.stringify(names));
 });
