b1ca2dfaeb4c2bb73845893a361c8876cb32f35f
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-06 15:20:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,27 @@ var button=document.getElementById("counter");
 
 var counter=0;
 button.onclick=function(){
-    counter=counter+1;
-    var span=
-    document.getElementById("count");
-    span.innerHTML=counter.toString();
+    
+    // create a request
+    var request=XMLHttpRequest();
+    
+    
+    // capture response and store it in a variable
+    request.onreadystatechange=function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           if(request.status===200){
+               var counter=request.responseText;
+             var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+               
+           }
+       } 
+        
+    };
+    
+    //make a request
+    request.open('GET','http://swathikandooree.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
 
 
