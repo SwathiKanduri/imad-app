a724eb959ee898034df9d5e9528d7c77d8d4ca62
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 00:34:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -98,7 +98,7 @@ var inputdrw=inpdraw.value;
     
     };
     
-    request.open('GET','http://swathikandooree.imad.hasura-app.io/drawing/submitdrw-inputdrw?inputdrw='+inputdrw,true);
+    request.open('GET','http://swathikandooree.imad.hasura-app.io/drawing/submitdrw',true);
     request.send(null);
     
 };
