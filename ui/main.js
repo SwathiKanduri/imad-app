9d62d792f1efe377538a68482ad26338c83c2c4a
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 16:18:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -84,10 +84,10 @@ var inputdrw=inpdraw.value;
     request.onreadystatechange=function(){
        if(request.readyState===XMLHttpRequest.DONE){
            if(request.status===200){
-              var comments= request.responseText;
+            //  var comments= request.responseText;
               comments=inputdrw;
     var division=document.getElementById('spndrw');
-    // division.innerHTML=comments;
+    division.innerHTML=comments;
   
                
            }
