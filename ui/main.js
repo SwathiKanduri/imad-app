a77a4acd12230bd11b1766926dc4ac28416e491e
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 14:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ var name=nameInput.value;
        if(request.readyState===XMLHttpRequest.DONE){
            if(request.status===200){
                
-               //var names=request.responseText;
+               var names=request.responseText;
                names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++){
