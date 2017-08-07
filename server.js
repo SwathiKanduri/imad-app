2c1f68f9d1486a4c462773d99dd1ec255ebf8bac
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 15:11:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,10 +67,11 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 
- var comments='';
+ var comments=[];
  app.get('/drawing/submitdrw-inputdrw', function (req, res) {
   var inputdrw=req.query.inputdrw;
-   res.send(comments);
+  comments.push(inputdrw);
+  res.send(JSON.stringify(comments));
   
 }); 
 
