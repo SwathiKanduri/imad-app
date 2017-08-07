0b174b11f0ac66307ba0bc6306f3d1560fe993f1
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 11:01:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -85,7 +85,7 @@ var inputdrw=inpdraw.value;
            if(request.status===200){
               var comments= request.responseText;
           
-    comments=inputdrw;
+    inputdrw+=comments;
     var division=document.getElementById('spndrw');
    division.innerHTML=comments;
   
