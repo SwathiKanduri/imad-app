196f381570663df5e5e50721139080b2c73976c1
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 10:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -83,12 +83,7 @@ var inputdrw=inpdraw.value;
        if(request.readyState===XMLHttpRequest.DONE){
            if(request.status===200){
               var comments= request.responseText;
-          /*     var names=request.responseText;
-               names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    } */
+          
     comments+=inputdrw;
     var division=document.getElementById('spndrw');
    division.innerHTML=comments;
@@ -100,7 +95,7 @@ var inputdrw=inpdraw.value;
     
     };
     
-    request.open('GET','http://swathikandooree.imad.hasura-app.io/drawing',true);
+    request.open('GET','http://swathikandooree.imad.hasura-app.io/drawing/submitdrw?comments',true);
     request.send(null);
     
 };
