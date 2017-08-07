3d540a128a8f7f166c1cb1fca408a1a81e645fa3
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-07 15:12:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,9 +76,9 @@ app.get('/submit-name', function (req, res) {
 }); 
 
 
-app.get('/drawing', function (req, res) {
+/* app.get('/drawing', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'drawing.html'));
-}); 
+}); */
 
 
 
