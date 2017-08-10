c9be962c9941268a817b007ec0f653d5198d6ed5
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-10 16:09:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
-  res.send(creatingtemplate(infos[articleName]));
+  res.send(creatingtemplate(content[articleName]));
 });
 
 
