7fae1bb2f43e05bf9d726050507f510e379e3e14
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-13 15:15:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(morgan('combined'));
  var content={
  'article-one' : {
     title:'Article-one',
+    date : 'Aug 13 2017' ,
        content: ` <p align="left" > <font class=art1info>
                ARTICLE ONE Pablo Picasso rightly said that every child is a born artist, the problem is to retain that artist within themselves. This universe is full of art and inspiration, that is what everyone can see, but it takes a really talented and visionary artist to pick paint and brush and illustrate their thoughts, visions and this beautiful universe into paintings. 
             </font>
@@ -25,6 +26,7 @@ app.use(morgan('combined'));
 
  'article-two' :{
     title:'Article-two' ,
+    date : 'Aug 13 2017',
        content: ` <p align="center"> <font class=art2info> ARTICLE TWO Drawing is a form of visual art in which a person uses various drawing instruments to mark paper or another two-dimensional.Drawing is a form of visual art in which a person uses various drawing instruments to mark paper or another two-dimensional medium
             Drawing is a form of visual art in which a person uses various drawing instruments to mark paper or another two-dimensional
             Drawing is a form of visual art in which a person uses various drawing instruments to mark paper or another two-dimensional
