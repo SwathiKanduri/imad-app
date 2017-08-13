e3b22e146f4f345ce6f64659c5a252634d2f51d2
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-13 15:29:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,27 @@ app.get('/test-db', function(req,res) {
 });
 
 
+app.get('/articles/:articleName', function (req, res) {
+  
+  //  var articleName=req.params.articleName;
+    pool.query("select * from article where title="+req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString(()));
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send("no rows with given title found");
+            }
+            else{
+                var articleData=result.rows[0];
+                res.send(creatingtemplate(articleData));
+            }
+        }
+    });
+    
+  
+});
+
 
 var counter=0;
 
