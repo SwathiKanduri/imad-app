c74387f09a94437be32dc81e8f36790d21fe802c
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2017-08-19 16:41:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,9 +143,9 @@ app.post('/login',function(req,res){
 app.get('/check-login',function(req,res){
    
     if(req.session && req.session.auth && req.session.auth.userId){
-        req.send('you are logged in. session id :' + req.session.auth.userId.toString());
+        res.send('you are logged in. session id :' + req.session.auth.userId.toString());
     } else{
-        req.send('you are not logged in ! ');
+        res.send('you are not logged in ! ');
     }
     
 });
